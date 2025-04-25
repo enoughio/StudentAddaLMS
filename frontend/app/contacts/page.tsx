193919fc8cbe5d93cd015f30962a059da7c046ca
joohirao <joohirao0413@gmail.com>
2025-04-26 00:55:24 +0530

    completed home page and contact us page

--- frontend/app/contacts/page.tsx
+++ frontend/app/contacts/page.tsx
@@ -1,9 +1,10 @@
 import ContactPage from "@/components/contact";
-
+import Navbar from "@/components/navbar";
 import React from 'react'
 export default function Contact(){
 
     return (
+        
 
         <ContactPage/>
 
